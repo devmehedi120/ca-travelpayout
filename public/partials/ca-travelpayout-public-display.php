821b4ebdc9df21bfd81cb7696105c7fd8f90d891
devmehedi120 <personal.mehedi@outlook.com>
2024-01-10 16:31:02 +0600

    final

--- public/partials/ca-travelpayout-public-display.php
+++ public/partials/ca-travelpayout-public-display.php
@@ -112,12 +112,12 @@
                                         <input id="selectcurrency" v-model="selectedCurrency" @focus="showCurDropdown"
                                             class="custom-select-input"
                                             :placeholder="selectedCurrency?selectedCurrency:currentCurrencyCode">
-                                        <span class="arrowSvg"><svg width="20px" height="20px" viewBox="0 0 1024 1024"
+                                        <!-- <span class="arrowSvg"><svg width="20px" height="20px" viewBox="0 0 1024 1024"
                                                 class="icon " version="1.1" xmlns="http://www.w3.org/2000/svg">
                                                 <path
                                                     d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z"
                                                     fill="#000" />
-                                            </svg></span>
+                                            </svg></span> -->
                                         <div v-if="showCurOptions" class="custom-select-dropdown">
                                             <div v-for="currency in filteredCurrency" :key="currency"
                                                 class="custom-select-option" @click="selectCurrency(currency)">
@@ -204,12 +204,12 @@
                                         <input id="selectcurrency" v-model="selectedCurrency" @focus="showCurDropdown"
                                             class="custom-select-input"
                                             :placeholder="selectedCurrency?selectedCurrency:currentCurrencyCode">
-                                        <span class="arrowSvg"><svg width="20px" height="20px" viewBox="0 0 1024 1024"
+                                        <!-- <span class="arrowSvg"><svg width="20px" height="20px" viewBox="0 0 1024 1024"
                                                 class="icon " version="1.1" xmlns="http://www.w3.org/2000/svg">
                                                 <path
                                                     d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z"
                                                     fill="#000" />
-                                            </svg></span>
+                                            </svg></span> -->
                                         <div v-if="showCurOptions" class="custom-select-dropdown">
                                             <div v-for="currency in filteredCurrency" :key="currency"
                                                 class="custom-select-option" @click="selectCurrency(currency)">
